dbd7120c8d80a15023267d4ede5164d84c93fc08
alirizakaygusuz <ali.rizakaygusuz@hotmail.com>
2025-09-05 18:49:49 +0300

    Initial commit

---
+++ src/main/java/com/alirizakaygusuz/redis_rate_limiter/RedisratelimiterApplication.java
@@ -0,0 +1,13 @@
+package com.alirizakaygusuz.redis_rate_limiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisratelimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisratelimiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alirizakaygusuz/redis_rate_limiter/RedisratelimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alirizakaygusuz.redis_rate_limiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisratelimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
